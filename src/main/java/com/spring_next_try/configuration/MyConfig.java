62b7763dbae2d3944c4ee977318689123a41cd24
Павел Климов <klimovpavel62@gmail.com>
2022-04-13 12:09:27 +0300

    1

--- src/main/java/com/spring_next_try/configuration/MyConfig.java
+++ src/main/java/com/spring_next_try/configuration/MyConfig.java
@@ -1,8 +1,48 @@
 package com.spring_next_try.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
 
 @Configuration
+@ComponentScan(basePackages = "com.spring_next_try")
+@EnableWebMvc
 public class MyConfig {
+
+    @Bean
+    public DataSource dataSource (){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuserbestuser1");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.spring_next_try.entity");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
+        return sessionFactory;
+    }
 }
