b0f09b89c5963443d82c4595b2ac1f0c86217cc1
Павел Климов <klimovpavel62@gmail.com>
2022-04-13 12:19:34 +0300

    1

--- src/main/java/com/spring_next_try/configuration/MyConfig.java
+++ src/main/java/com/spring_next_try/configuration/MyConfig.java
@@ -5,7 +5,9 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
@@ -15,6 +17,7 @@ import java.util.Properties;
 @Configuration
 @ComponentScan(basePackages = "com.spring_next_try")
 @EnableWebMvc
+@EnableTransactionManagement
 public class MyConfig {
 
     @Bean
@@ -44,5 +47,12 @@ public class MyConfig {
         sessionFactory.setHibernateProperties(hibernateProperties);
 
         return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(){
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
     }
 }
---
+++ src/main/java/com/spring_next_try/configuration/MyWebIn.java
@@ -0,0 +1,4 @@
+package com.spring_next_try.configuration;
+
+public class MyWebIn {
+}
