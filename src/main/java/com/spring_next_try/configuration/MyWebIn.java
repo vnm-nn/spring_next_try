f72658c9f9ee329b0b32e1aa6caa6d102cd973ef
Павел Климов <klimovpavel62@gmail.com>
2022-04-13 16:00:33 +0300

    1

--- src/main/java/com/spring_next_try/configuration/MyWebIn.java
+++ src/main/java/com/spring_next_try/configuration/MyWebIn.java
@@ -1,4 +1,20 @@
 package com.spring_next_try.configuration;
 
-public class MyWebIn {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebIn extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
---
+++ src/main/java/com/spring_next_try/controller/RestController.java
@@ -0,0 +1,5 @@
+package com.spring_next_try.controller;
+
+@RestController
+public class RestController {
+}
